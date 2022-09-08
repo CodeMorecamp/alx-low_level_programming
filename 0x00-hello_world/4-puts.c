a7d3ebc45403f0f472e554697a7eef0c4407140e
Olabiyi Femi Emmanuel <codemorecamp@gmail.com>
2022-09-08 10:30:48 -0700

    putssss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-#!/bin/bash
-#include<studio.h>
-int main(void) {
-puts("\"Programming is like buildingg a multilingual puzzle");
-return (0);
+#include <studio.h>
+/** 
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{puts ("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
