f8738c23ac6f86a0b821dc7489f85a672a3e6af2
Olabiyi Femi Emmanuel <codemorecamp@gmail.com>
2022-09-13 20:13:19 -0700

    Print another Alohabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-int alphabet;
-int count;
+	int a, b;
 
-
-count = 0;
-while (count < 10)
-{
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
-{
-_putchar(alphabet);
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
 }
-
-count++;
-_putchar('\n');
-}
-
-
-}}
