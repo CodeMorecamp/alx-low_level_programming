cc8fdb17de5685f7b5abcd499bc242b89ba3efde
Olabiyi Femi Emmanuel <codemorecamp@gmail.com>
2022-09-21 20:12:38 -0700

    Reversed Array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -18,24 +18,4 @@ void reverse_array(int *a, int n)
 		a[j] = tmp;
 		j--;
 	}
-#include "main.h"
-
-/**
- *reverse_array - reverse an array.
- *@a: array.
- *@n: array's length.
- */
-
-void reverse_array(int *a, int n)
-{
-	int i, j, tmp;
-
-	j = n - 1;
-	for (i = 0; i < n / 2; i++)
-	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-		j--;
-	}
 }
