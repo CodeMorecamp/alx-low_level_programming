218c7aaec81c7ebb99d1f4d59da96b069d6dae92
Olabiyi Femi Emmanuel <codemorecamp@gmail.com>
2022-10-20 11:03:55 -0700

    Print the d list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,22 +3,23 @@
 
 /**
  * print_list - A function that prints a linked list
- * @h: pointer t  head of list
+ * @h: The list_t list.
+ *
  * Return: number of nodes in list as size_t
  */
 size_t print_list(const list_t *h)
 {
-	size_t node;
+	size_t nodes = 0;
 
-	nelem = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
-		node++;
 	}
-	return (node);
+	return (nodes);
 }
